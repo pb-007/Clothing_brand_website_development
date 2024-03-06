28530f7ee360322a70c79085d6a6ba76c93577d2
pb-007 <pratikbhoj007@gmail.com>
2024-03-06 16:25:39 +0530

    funtions

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//let rightNum = 7;
+//let num = prompt("guess a number")
+//while(num != rightNum){
+//    num = prompt("guess a number. guess it again :")
+//}
+
+//console.log("congratulations")
+//arr = [1,2, 3,4]
+//for(let idx = 0; idx < arr.length; idx++){
+//  console.log(arr[idx]);
+//}
+//let sum = 0;
+//for(let val of arr){
+ //   sum += val;
+//}
+//let average = sum/arr.length;
+//console.log(average)
+let companies = ["bloomberg", "microsoft", "uber", "google", "IBM", "Netflix"]
+//newCompanies = companies.shift();
+//companies.splice(2,1,"ola");
+//console.log(companies);
+companies.push("Amazon");
+console.log(companies);
